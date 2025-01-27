4415010a05ada296ead640f15ea07c0d3f55539e
Tomisin <ota231@nyu.edu>
2025-01-27 18:49:42 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+import io
+from pyscript import document
+
+def execute_code(event):
+    user_code = document.querySelector("#code-editor").value
+
+    #redirect stdout
+    old_stdout = sys.stdout
+    new_stdout = io.StringIO()
+    sys.stdout = new_stdout
+
+    try:
+        exec(user_code)
+    except Exception as e:
+        new_stdout.write(str(e))
+    finally:
+        sys.stdout = old_stdout
+
+    output_content = new_stdout.getvalue()
+
+    document.querySelector("#output").innerText = output_content
